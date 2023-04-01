5680b650705c6336d16f374a4391b28e4d99201f
Pablo Tilotta <ptilotta@aper.com>
2023-04-01 19:43:34 -0300

    Ultimo Commit

---
+++ bd/signup.go
@@ -0,0 +1,31 @@
+package bd
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/ptilotta/gambituser/models"
+	"github.com/ptilotta/gambituser/tools"
+)
+
+func SignUp(sig models.SignUp) error {
+	fmt.Println("Comienza Registro")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySQL() + "')"
+	fmt.Println(sentencia)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("SignUp > Ejecución Exitosa")
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+	"github.com/ptilotta/gambituser/awsgo"
+	"github.com/ptilotta/gambituser/bd"
+	"github.com/ptilotta/gambituser/models"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
+
+	awsgo.InicializoAWS()
+
+	if !ValidoParametros() {
+		fmt.Println("Error en los parámetros. debe enviar 'SecretName'")
+		err := errors.New("error en los parámetros debe enviar SecretName")
+		return event, err
+	}
+
+	var datos models.SignUp
+
+	for row, att := range event.Request.UserAttributes {
+		switch row {
+		case "email":
+			datos.UserEmail = att
+			fmt.Println("Email = " + datos.UserEmail)
+		case "sub":
+			datos.UserUUID = att
+			fmt.Println("Sub = " + datos.UserUUID)
+		}
+	}
+
+	err := bd.ReadSecret()
+	if err != nil {
+		fmt.Println("Error al leer el Secret " + err.Error())
+		return event, err
+	}
+
+}
+
+func ValidoParametros() bool {
+	var traeParametro bool
+	_, traeParametro = os.LookupEnv("SecretName")
+	return traeParametro
+}

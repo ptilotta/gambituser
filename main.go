47bc9effc7be1857631e5faa0126fa4f2acd8d57
Pablo Tilotta <ptilotta@aper.com>
2023-04-02 13:46:46 -0300

    Ultimo Commit

--- main.go
+++ main.go
@@ -46,6 +46,8 @@ func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostCo
 		return event, err
 	}
 
+	err = bd.SignUp(datos)
+	return event, err
 }
 
 func ValidoParametros() bool {

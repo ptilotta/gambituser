7151e9a980de2bd5a42c2193f17d8a13a8de699f
Pablo Tilotta <ptilotta@aper.com>
2023-04-02 14:06:41 -0300

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -3,6 +3,6 @@ git commit -m "Ultimo Commit"
 git push
 set GOOS=linux
 set GOARCH=amd64
-go build main.go 
+go build main.go
 del main.zip
-tar.exe -a -cf main.zip main 
+tar.exe -a -cf main.zip main
